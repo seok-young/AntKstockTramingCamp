8db14762e0f9309effa9d63e72ccfef63d7124c5
seok-young <nsy7160@gmail.com>
2026-01-11 11:16:02 +0900

    feat:scan_sell

--- app/scripts/test.py
+++ app/scripts/test.py
@@ -4,7 +4,6 @@ import pandas as pd
 from app.service.analysis import (
     fetch_analysis,
 )
-from app.core.database import Base,engine
 
 from app.service.recommend import (
     get_recent_analysis,
@@ -22,12 +21,16 @@ from app.service.collector import (
     save_price_to_db
     )
 
-from app.model import TradeInput
 
 from app.service.trade_manager import (
     TradeManager
 )
 
+from app.service.scanner import (
+    scan_sell
+)
+
+from app.model import TradeInput
 from app.core.database import SessionLocal,Base,engine
 from app.core.config import settings
 
@@ -35,66 +38,69 @@ from app.core.config import settings
 
 if __name__ == '__main__':
     
-    session = SessionLocal()
+    # session = SessionLocal()
 
-    # 매수 조건 뜬 애들 데려다가(모두 샀다고 가정) 매도 조건 뜨는 지 비교
-    # 가장 과거 rec 뜬 애들 7개 대상으로 매도 따져보자
-    query = f"""
-        SELECT *
-        FROM (
-            SELECT *,
-                ROW_NUMBER() OVER (PARTITION BY ticker_symbol ORDER BY base_date ASC) as row_num
-            FROM recommendation
-        ) as ranked_data
-        WHERE row_num = 1
-        ORDER BY base_date ASC
-        LIMIT 7;
-    """
+    # # 매수 조건 뜬 애들 데려다가(모두 샀다고 가정) 매도 조건 뜨는 지 비교
+    # # 가장 과거 rec 뜬 애들 7개 대상으로 매도 따져보자
+    # query = f"""
+    #     SELECT *
+    #     FROM (
+    #         SELECT *,
+    #             ROW_NUMBER() OVER (PARTITION BY ticker_symbol ORDER BY base_date ASC) as row_num
+    #         FROM recommendation
+    #     ) as ranked_data
+    #     WHERE row_num = 1
+    #     ORDER BY base_date ASC
+    #     LIMIT 7;
+    # """
     
-    target = pd.read_sql(query, con=engine)
-    target['share'] = None
-    target['price'] = target['price'].astype(float)
-    # print(target.head())
+    # target = pd.read_sql(query, con=engine)
+    # target['share'] = None
+    # target['price'] = target['price'].astype(float)
+    # # print(target.head())
 
 
 
-    """"
-    To-Do
-    0. 몇 주를 사야되는 지 계산하기 V
-    1. 포트폴리오에 저장하기       V
-    2. account_history에 기록    V
-    3. 매도 조건 따져보기
-    4. 매도 API 테스트
-    """
+    # """"
+    # To-Do
+    # 0. 몇 주를 사야되는 지 계산하기 V
+    # 1. 포트폴리오에 저장하기       V
+    # 2. account_history에 기록    V
+    # 3. 매도 조건 따져보기
+    # 4. 매도 API 테스트
+    # """
 
-    # 1. 몇 주 사야 되는지 계산
-    target.loc[target['price'] >= 100000, 'share'] = 1
-    target.loc[target['price'] < 100000, 'share'] = 100000 // target['price']
+    # # 1. 몇 주 사야 되는지 계산
+    # target.loc[target['price'] >= 100000, 'share'] = 1
+    # target.loc[target['price'] < 100000, 'share'] = 100000 // target['price']
 
-    # print(target.to_string())
+    # # print(target.to_string())
 
-    # 2. 포폴 저장
-    manager = TradeManager(session)
+    # # 2. 포폴 저장
+    # manager = TradeManager(session)
     
-    cashinput = {
-        'amount' : 1000000,
-        'transaction_type' : 'DEPOSIT'
-    }
+    # cashinput = {
+    #     'amount' : 1000000,
+    #     'transaction_type' : 'DEPOSIT'
+    # }
 
-    manager.record_cash_flow(cashinput)
+    # manager.record_cash_flow(cashinput)
 
 
-    temp =[]
-    for _,row in target.iterrows():
-        trade = TradeInput(
-            ticker_symbol=row['ticker_symbol'],
-            rec_id=row['id'],
-            qty=row['share'],
-            price=row['price'],
-            transaction_type=row['signal_type']
-        )
-        try :
-            manager.execute_trade(trade)
-        except Exception as e:
-            print(f"Error during executing trade [{e}]")
-        # print(trade)
+    # temp =[]
+    # for _,row in target.iterrows():
+    #     trade = TradeInput(
+    #         ticker_symbol=row['ticker_symbol'],
+    #         rec_id=row['id'],
+    #         qty=row['share'],
+    #         price=row['price'],
+    #         transaction_type=row['signal_type']
+    #     )
+    #     try :
+    #         manager.execute_trade(trade)
+    #     except Exception as e:
+    #         print(f"Error during executing trade [{e}]")
+    #     # print(trade)
+
+    target=scan_sell()
+    print(target)
--- app/service/scanner.py
+++ app/service/scanner.py
@@ -1,4 +1,6 @@
 from app.core.database import SessionLocal, Base, engine
+from app.model import Portfolio
+
 
 """"
 매수 대상
@@ -13,12 +15,17 @@ def scan_buy():
 포트폴리오에 담겨있는 종목
 """
 def scan_sell():
-#     session = SessionLocal()
-#     query = f"""
-#     SELECT ticker_symbol FROM portfolio 
-#     WHERE      
-
-# """
-    return None
+    session = SessionLocal()
+    try:
+        result = session.query(Portfolio.ticker_symbol)\
+                        .filter(Portfolio.is_active == 1)\
+                        .all()        
+        return [r[0] for r in result]
+    except Exception as e:
+        print(f"Error during scanning sell [{e}]")
+        return None    
+    finally:
+        session.close()
+    
 
 
